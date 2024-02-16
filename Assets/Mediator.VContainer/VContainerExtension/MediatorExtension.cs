bbd73c7ac8e62af51ef6830bef51b573c0aee40c
Leon Langhans <leon.langhans@straumann.com>
2024-02-16 15:45:49 +0100

    Cleanup of deprecated methods and extending registration

--- Assets/Mediator.VContainer/VContainerExtension/MediatorExtension.cs
+++ Assets/Mediator.VContainer/VContainerExtension/MediatorExtension.cs
@@ -12,9 +12,8 @@ namespace Straumann.Mediator
     {
         private static MessagePipeOptions _options;
 
-        #region RequestHandlers
+        #region RequestHandlerRegistration
 
-        // Async RequestHandlers
         public static RegistrationBuilder RegisterAsyncMediatRRequestHandler<TRequest, TResponse, TAsyncRequestHandler>(
             this IContainerBuilder builder)
             where TAsyncRequestHandler : IAsyncRequestHandler<TRequest, TResponse> where TRequest : IRequest
@@ -43,7 +42,8 @@ namespace Straumann.Mediator
 
         #endregion
 
-        #region Notification
+        #region NotificationRegistration
+        
         public static void RegisterNotificationType<TNotificationType>(
             this IContainerBuilder builder)
             where TNotificationType : INotification
@@ -58,16 +58,65 @@ namespace Straumann.Mediator
             _options ??= builder.RegisterMessagePipe();
             builder.RegisterMessageBroker<TNotificationType>(_options);
         }
+        
         #endregion
 
-        #region SyncNotificationHandlers
+        #region NotificationHandlerRegistration
+        
         public static RegistrationBuilder RegisterNotificationHandler<TNotification, TNotificationHandler>(this IContainerBuilder builder, Lifetime lifetime = Lifetime.Scoped)
             where TNotification : INotification
             where TNotificationHandler : IAsyncNotificationHandler<TNotification> 
         {
-            return builder.BindHandlersInBuildCallback<TNotification, TNotificationHandler>()
+            return builder
+                .BindHandlersInBuildCallback<TNotification, TNotificationHandler>()
                 .Register<IAsyncNotificationHandler<TNotification>, TNotificationHandler>(lifetime)
                 .AsSelf();
+        }
+        
+        public static RegistrationBuilder RegisterNotificationHandler<TNotification1, TNotification2, TNotificationHandler>(
+            this IContainerBuilder builder, Lifetime lifetime = Lifetime.Scoped)
+            where TNotification1 : INotification where TNotification2 : INotification
+            where TNotificationHandler : IAsyncNotificationHandler<TNotification1>,IAsyncNotificationHandler<TNotification2>
+        {
+            return builder
+                .RegisterNotificationHandler<TNotification1, TNotificationHandler>(lifetime)
+                .AsAdditionalNotificationHandler<TNotification2, TNotificationHandler>(builder);
+        }
+        
+        public static RegistrationBuilder RegisterNotificationHandler<TNotification1, TNotification2, TNotification3, TNotificationHandler>(
+            this IContainerBuilder builder, Lifetime lifetime = Lifetime.Scoped)
+            where TNotification1 : INotification where TNotification2 : INotification where TNotification3 : INotification
+            where TNotificationHandler : IAsyncNotificationHandler<TNotification1>, IAsyncNotificationHandler<TNotification2>, IAsyncNotificationHandler<TNotification3>
+        {
+            return builder
+                .RegisterNotificationHandler<TNotification1, TNotificationHandler>(lifetime)
+                .AsAdditionalNotificationHandler<TNotification2, TNotificationHandler>(builder)
+                .AsAdditionalNotificationHandler<TNotification3, TNotificationHandler>(builder);
+        }
+        
+        public static RegistrationBuilder RegisterNotificationHandler<TNotification1, TNotification2, TNotification3, TNotification4, TNotificationHandler>(
+            this IContainerBuilder builder, Lifetime lifetime = Lifetime.Scoped)
+            where TNotification1 : INotification where TNotification2 : INotification where TNotification3 : INotification where TNotification4 : INotification
+            where TNotificationHandler : IAsyncNotificationHandler<TNotification1>, IAsyncNotificationHandler<TNotification2>, IAsyncNotificationHandler<TNotification3>, IAsyncNotificationHandler<TNotification4>
+        {
+            return builder
+                .RegisterNotificationHandler<TNotification1, TNotificationHandler>(lifetime)
+                .AsAdditionalNotificationHandler<TNotification2, TNotificationHandler>(builder)
+                .AsAdditionalNotificationHandler<TNotification3, TNotificationHandler>(builder)
+                .AsAdditionalNotificationHandler<TNotification4, TNotificationHandler>(builder);
+        }
+        
+        public static RegistrationBuilder RegisterNotificationHandler<TNotification1, TNotification2, TNotification3, TNotification4, TNotification5, TNotificationHandler>(
+            this IContainerBuilder builder, Lifetime lifetime = Lifetime.Scoped)
+            where TNotification1 : INotification where TNotification2 : INotification where TNotification3 : INotification where TNotification4 : INotification where TNotification5 : INotification
+            where TNotificationHandler : IAsyncNotificationHandler<TNotification1>, IAsyncNotificationHandler<TNotification2>, IAsyncNotificationHandler<TNotification3>, IAsyncNotificationHandler<TNotification4>, IAsyncNotificationHandler<TNotification5>
+        {
+            return builder
+                .RegisterNotificationHandler<TNotification1, TNotificationHandler>(lifetime)
+                .AsAdditionalNotificationHandler<TNotification2, TNotificationHandler>(builder)
+                .AsAdditionalNotificationHandler<TNotification3, TNotificationHandler>(builder)
+                .AsAdditionalNotificationHandler<TNotification4, TNotificationHandler>(builder)
+                .AsAdditionalNotificationHandler<TNotification5, TNotificationHandler>(builder);
         }
 
         private static IContainerBuilder BindHandlersInBuildCallback<TNotification, TNotificationHandler>(this IContainerBuilder builder)
@@ -90,15 +139,6 @@ namespace Straumann.Mediator
             });
             return builder;
         }
-
-        public static RegistrationBuilder RegisterNotificationHandler<TNotification1, TNotification2, TNotificationHandler>(
-            this IContainerBuilder builder, Lifetime lifetime = Lifetime.Scoped)
-            where TNotification1 : INotification where TNotification2 : INotification
-            where TNotificationHandler : IAsyncNotificationHandler<TNotification1>,IAsyncNotificationHandler<TNotification2>
-        {
-            return builder.RegisterNotificationHandler<TNotification1, TNotificationHandler>(lifetime)
-                .AsAdditionalNotificationHandler<TNotification2, TNotificationHandler>(builder);
-        }
         
         private static RegistrationBuilder AsAdditionalNotificationHandler<TNotification, TNotificationHandler>(this RegistrationBuilder registrationBuilder, IContainerBuilder containerBuilder) 
             where TNotification : INotification 
@@ -106,122 +146,6 @@ namespace Straumann.Mediator
         {
             containerBuilder.BindHandlersInBuildCallback<TNotification, TNotificationHandler>();
             return registrationBuilder.As<IAsyncNotificationHandler<TNotification>>();
-        }
-
-        #endregion
-
-        #region Async NotificationHandlers
-        public static RegistrationBuilder RegisterAsyncMediatRNotificationHandler<TNotification, TAsyncNotificationHandler>(
-            this IContainerBuilder builder, Lifetime lifetime = Lifetime.Scoped)
-            where TAsyncNotificationHandler : IAsyncNotificationHandler<TNotification> where TNotification : INotification
-        {
-            builder.RegisterBuildCallback(container =>
-            {
-                var bagBuilder = DisposableBag.CreateBuilder();
-
-                container.BindAsyncHandler<TNotification>(bagBuilder);
-
-                var handler = container.Resolve<IAsyncNotificationHandler<TNotification>>();
-                handler.NotificationDisposable?.AddTo(bagBuilder);
-                handler.NotificationDisposable = bagBuilder.Build();
-            });
-            return builder.Register<IAsyncNotificationHandler<TNotification>, TAsyncNotificationHandler>(lifetime);
-        }
-
-        public static RegistrationBuilder RegisterAsyncMediatRNotificationHandler<T1, T2, TAsyncNotificationHandler>(
-            this IContainerBuilder builder, Lifetime lifetime = Lifetime.Scoped)
-            where TAsyncNotificationHandler : IAsyncNotificationHandler<T1> 
-            where T1 : INotification where T2 : INotification
-        {
-            builder.RegisterBuildCallback(container =>
-            {
-                var bagBuilder = DisposableBag.CreateBuilder();
-
-                container.BindAsyncHandler<T1>(bagBuilder);
-                container.BindAsyncHandler<T2>(bagBuilder);
-
-                var handler = container.Resolve<IAsyncNotificationHandler<T1>>();
-                handler.NotificationDisposable?.AddTo(bagBuilder);
-                handler.NotificationDisposable = bagBuilder.Build();
-            });
-            
-            return builder.Register<TAsyncNotificationHandler>(lifetime).As<IAsyncNotificationHandler<T1>, IAsyncNotificationHandler<T2>>();
-        }
-        
-        public static RegistrationBuilder RegisterAsyncMediatRNotificationHandler<T1, T2, T3, TAsyncNotificationHandler>(
-            this IContainerBuilder builder, Lifetime lifetime = Lifetime.Scoped)
-            where TAsyncNotificationHandler : IAsyncNotificationHandler<T1> 
-            where T1 : INotification where T2 : INotification where T3 : INotification
-        {
-            builder.RegisterBuildCallback(container =>
-            {
-                var bagBuilder = DisposableBag.CreateBuilder();
-
-                container.BindAsyncHandler<T1>(bagBuilder);
-                container.BindAsyncHandler<T2>(bagBuilder);
-                container.BindAsyncHandler<T3>(bagBuilder);
-
-                var handler = container.Resolve<IAsyncNotificationHandler<T1>>();
-                handler.NotificationDisposable?.AddTo(bagBuilder);
-                handler.NotificationDisposable = bagBuilder.Build();
-            });
-
-            return builder.Register<TAsyncNotificationHandler>(lifetime).As<IAsyncNotificationHandler<T1>, IAsyncNotificationHandler<T2>, IAsyncNotificationHandler<T3>>();
-        }
-
-        public static RegistrationBuilder RegisterAsyncMediatRNotificationHandler<T1, T2, T3, T4, TAsyncNotificationHandler>(
-            this IContainerBuilder builder, Lifetime lifetime = Lifetime.Scoped)
-            where TAsyncNotificationHandler : IAsyncNotificationHandler<T1> 
-            where T1 : INotification where T2 : INotification where T3 : INotification where T4 : INotification
-        {
-            builder.RegisterBuildCallback(container =>
-            {
-                var bagBuilder = DisposableBag.CreateBuilder();
-
-                container.BindAsyncHandler<T1>(bagBuilder);
-                container.BindAsyncHandler<T2>(bagBuilder);
-                container.BindAsyncHandler<T3>(bagBuilder);
-                container.BindAsyncHandler<T4>(bagBuilder);
-
-                var handler = container.Resolve<IAsyncNotificationHandler<T1>>();
-                handler.NotificationDisposable?.AddTo(bagBuilder);
-                handler.NotificationDisposable = bagBuilder.Build();
-            });
-            
-            return builder.Register<TAsyncNotificationHandler>(lifetime).As<IAsyncNotificationHandler<T1>, IAsyncNotificationHandler<T2>, IAsyncNotificationHandler<T3>, IAsyncNotificationHandler<T4>>();
-        }
-
-        public static RegistrationBuilder RegisterAsyncMediatRNotificationHandler<T1, T2, T3, T4, T5, TAsyncNotificationHandler>(
-            this IContainerBuilder builder, Lifetime lifetime = Lifetime.Scoped)
-            where TAsyncNotificationHandler : IAsyncNotificationHandler<T1> 
-            where T1 : INotification where T2 : INotification where T3 : INotification where T4 : INotification where T5 : INotification
-        {
-            builder.RegisterBuildCallback(container =>
-            {
-                var bagBuilder = DisposableBag.CreateBuilder();
-
-                container.BindAsyncHandler<T1>(bagBuilder);
-                container.BindAsyncHandler<T2>(bagBuilder);
-                container.BindAsyncHandler<T3>(bagBuilder);
-                container.BindAsyncHandler<T4>(bagBuilder);
-                container.BindAsyncHandler<T5>(bagBuilder);
-
-                var handler = container.Resolve<IAsyncNotificationHandler<T1>>();
-                handler.NotificationDisposable?.AddTo(bagBuilder);
-                handler.NotificationDisposable = bagBuilder.Build();
-            });
-            
-            return builder.Register<TAsyncNotificationHandler>(lifetime).As(typeof(IAsyncNotificationHandler<T1>), typeof(IAsyncNotificationHandler<T2>), typeof(IAsyncNotificationHandler<T3>), typeof(IAsyncNotificationHandler<T4>), typeof(IAsyncNotificationHandler<T5>));
-        }
-        
-        private static void BindAsyncHandler<TNotification>(this IObjectResolver resolver, DisposableBagBuilder bagBuilder)
-            where TNotification : INotification
-        {
-            var notificationHandler = resolver.Resolve<IAsyncNotificationHandler<TNotification>>();
-            var subscriber = resolver.Resolve<IAsyncSubscriber<TNotification>>();
-            subscriber.Subscribe((notification, cancellationToken) 
-                    => notificationHandler.HandleAsync(notification, cancellationToken).AsUniTask())
-                .AddTo(bagBuilder);
         }
 
         #endregion
